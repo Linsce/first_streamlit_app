5c220a9fb38e1e6a34eaf9825a92bc629593cd5a
Linsce <31203821+Linsce@users.noreply.github.com>
2023-01-25 17:55:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
  
 streamlit.write('The user entered ', fruit_choice)
 
-#streamlit.stop()
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
